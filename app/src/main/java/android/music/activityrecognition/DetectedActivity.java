e6c1ae7a371726398abfc560b97d9d5ceff5dcf3
RaguRamanTB <aakash1956ragu@gmail.com>
2020-03-18 22:11:44 +0530

    Listing all the possible activities in a List view

---
+++ app/src/main/java/android/music/activityrecognition/DetectedActivity.java
@@ -0,0 +1,14 @@
+package android.music.activityrecognition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetectedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detected);
+    }
+}
--- app/src/main/java/android/music/activityrecognition/MainActivity.java
+++ app/src/main/java/android/music/activityrecognition/MainActivity.java
@@ -3,12 +3,82 @@ package android.music.activityrecognition;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.ListView;
+import android.app.PendingIntent;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
+import android.view.View;
+import com.google.android.gms.location.ActivityRecognitionClient;
+import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private Context mContext;
+    public static final String DETECTED_ACTIVITY = ".DetectedActivity";
+    private ActivityRecognitionClient mActivityRecognitionClient;
+    private ActivitiesAdapter mAdapter;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+        ListView detectedActivitiesListView = (ListView) findViewById(R.id.activities_listview);
+
+        ArrayList<DetectedActivity> detectedActivities = ActivityIntentService.detectedActivitiesFromJson(
+                PreferenceManager.getDefaultSharedPreferences(this).getString(
+                        DETECTED_ACTIVITY, ""));
+        mAdapter = new ActivitiesAdapter(this, detectedActivities);
+        detectedActivitiesListView.setAdapter(mAdapter);
+        mActivityRecognitionClient = new ActivityRecognitionClient(this);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(this);
+        updateDetectedActivitiesList();
+    }
+    @Override
+    protected void onPause() {
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+    public void requestUpdatesHandler(View view) {
+        Task<Void> task = mActivityRecognitionClient.requestActivityUpdates(
+                500,
+                getActivityDetectionPendingIntent());
+        task.addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void result) {
+                updateDetectedActivitiesList();
+            }
+        });
+    }
+    //Get a PendingIntent//
+    private PendingIntent getActivityDetectionPendingIntent() {
+//Send the activity data to our DetectedActivitiesIntentService class//
+        Intent intent = new Intent(this, ActivityIntentService.class);
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+    }
+    //Process the list of activities//
+    protected void updateDetectedActivitiesList() {
+        ArrayList<DetectedActivity> detectedActivities = ActivityIntentService.detectedActivitiesFromJson(
+                PreferenceManager.getDefaultSharedPreferences(mContext)
+                        .getString(DETECTED_ACTIVITY, ""));
+
+        mAdapter.updateActivities(detectedActivities);
+    }
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (s.equals(DETECTED_ACTIVITY)) {
+            updateDetectedActivitiesList();
+        }
     }
 }
